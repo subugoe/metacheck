815e90fcbf3154f74803f0d0275407eed53f4d6e
Maximilian Held <info@maxheld.de>
2020-12-09 20:08:26 +0100

    test sending email on ghactions

--- tests/testthat/test-email.R
+++ tests/testthat/test-email.R
@@ -2,3 +2,8 @@ test_that("email can be rendered", {
   # pretty bad test
   checkmate::expect_list(render_email(dois = tu_dois()[0:10]))
 })
+
+test_that("email can be send", {
+  skip_if_not(Sys.getenv("GITHUB_ACTIONS") == "true")
+  send_email(to = "info@maxheld.de", email = blastula::prepare_test_message())
+})

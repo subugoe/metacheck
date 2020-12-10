6d75449848a5cafeaaa456f3815c6f1883471603
Maximilian Held <info@maxheld.de>
2020-12-10 16:52:58 +0100

    attach excel results closes #44 opens #52
    This is a pretty bad hackfix that is untested and may easily break.
    A proper solution will require refactoring as per #44.

--- R/email.R
+++ R/email.R
@@ -1,7 +1,8 @@
-#' Send out email
+#' Render email
 #' @param dois Character vector of DOIs
+#' @param session_id Character vector to identify current shiny session
 #' @export
-render_email <- function(dois) {
+render_email <- function(dois, session_id = NULL) {
   cr <- get_cr_md(dois)
   my_df <- cr_compliance_overview(cr)
   email <- blastula::compose_email(
@@ -17,7 +18,8 @@ render_email <- function(dois) {
           cr_overview = my_df$cr_overview,
           cr_license = my_df$cc_license_check,
           cr_tdm = my_df$tdm,
-          cr_funder = my_df$funder_info
+          cr_funder = my_df$funder_info,
+          session_id = session_id
         )
       )
     )$html_html,
@@ -25,7 +27,19 @@ render_email <- function(dois) {
       "Email sent on ", format(Sys.time(), "%a %b %d %X %Y"), "."
     ))
   )
+  email <- add_attachment_xlsx(email, session_id = session_id)
   email
+}
+
+#' Add attachment to email
+#' @inheritParams blastula::add_attachment
+#' @export
+add_attachment_xlsx <- function(email, session_id = NULL) {
+  blastula::add_attachment(
+    email = email,
+    file = xlsx_path(session_id),
+    filename = "metadata_report.xlsx"
+  )
 }
 
 #' Send out email
@@ -46,4 +60,10 @@ send_email <- function(to, email, cc = "metacheck-support@sub.uni-goettingen.de"
       pass_envvar = "MAILJET_SMTP_PASSWORD"
     )
   )
+}
+
+#' Temp path to write xlsx to
+#' @inheritParams render_email
+xlsx_path <- function(session_id = NULL) {
+  fs::path_temp(paste0(session_id, "-license_df.xlsx"))
 }
--- tests/testthat/test-email.R
+++ tests/testthat/test-email.R
@@ -6,12 +6,12 @@ test_that("email can be rendered", {
 test_that("email can be send", {
   skip_if(Sys.getenv("MAILJET_SMTP_PASSWORD") == "")
   send_email(
-    to = "info@maxheld.de",
+    to = "held@sub.uni-goettingen.de",
     email = blastula::prepare_test_message(),
     cc = NULL
   )
   send_email(
-    to = "info@maxheld.de",
+    to = "held@sub.uni-goettingen.de",
     email = render_email(dois = tu_dois()[0:10]),
     cc = NULL
   )

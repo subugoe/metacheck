6583718046535092906fce30d71dc7a6a8fc9b56
Maximilian Held <info@maxheld.de>
2020-12-10 23:08:27 +0100

    improve UI closes #45

--- R/helpers.R
+++ R/helpers.R
@@ -3,3 +3,13 @@
 system_file2 <- function(...) {
   system.file(..., package = "metacheck")
 }
+
+#' Validate email
+#' @noRd
+is_valid_email <- function(x) {
+  grepl(
+    "^\\s*[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\s*$",
+    as.character(x),
+    ignore.case = TRUE
+  )
+}

df92c95a724c6dd66d94587ff1af80ed788b0380
Maximilian Held <info@maxheld.de>
2021-02-04 21:14:41 +0100

    remove bad test for cr md plus opens #82

--- tests/testthat/test-cr_auth.R
+++ tests/testthat/test-cr_auth.R
@@ -5,18 +5,6 @@ test_that("crossref metadata is enabled", {
   expect_equal(1, 1)
 })
 
-test_that("crossref metadata plus is faster", {
-  skip("Test does not work; times are often the same.")
-  with_mdplus <- bench::bench_time(
-    rcrossref::cr_agency(tu_dois[1:10], .progress = "text")
-  )["real"]
-  withr::local_envvar(.new = c("crossref_plus" = ""))
-  without_mdplus <- bench::bench_time(
-    rcrossref::cr_agency(tu_dois[1:10], .progress = "text")
-  )["real"]
-  expect_lte(with_mdplus, without_mdplus)
-})
-
 test_that("metacheck declares polite user agent", {
   expect_true(is_valid_email(Sys.getenv("crossref_email")))
 })

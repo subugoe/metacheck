8fe68686f23fa773e81a22a7b941b923dcf38d6b
Najko Jahn <najko.jahn@gmail.com>
2021-02-15 14:08:31 +0100

    upps

--- R/metrics_tdm.R
+++ R/metrics_tdm.R
@@ -35,7 +35,6 @@
 #' # as tibble
 #' tdm_metrics(out, .gt = FALSE)
 #' }
-<<<<<<< HEAD
 tdm_metrics <- function(.md = NULL, .gt = TRUE,  .color = "#9B0056") {
   if(is.null(.md) || !c("tdm", "cr_overview") %in% names(.md))
     stop("No TDM data available, get data using cr_compliance_overview()")

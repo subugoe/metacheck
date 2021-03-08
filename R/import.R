14fec4c5573c94e6655d50cda7148339bedbe9ca
Maximilian Held <info@maxheld.de>
2021-03-08 12:34:37 +0100

    enable caching everywhere closes #177

--- R/import.R
+++ R/import.R
@@ -1,20 +1,14 @@
 #' Get Crossref metadata from API
 #'
-#' @param dois character vector with DOIs
-#' @inheritParams rcrossref::cr_works
+#' @inheritParams cr_works2
 #' @family import
 #' @export
-get_cr_md <- function(dois,
-                      .progress = ifelse(interactive(), "text", "none")) {
-  assert_metacheckable(dois)
-  tt <- rcrossref::cr_works(dois = dois, .progress = .progress)[["data"]]
-  if (nrow(tt) != 0) {
-    out <- tt %>%
+get_cr_md <- function(x) {
+  assert_metacheckable(x)
+  tt <- cr_works2(x)
+  out <- tt %>%
     mutate(issued = lubridate::parse_date_time(issued, c("y", "ymd", "ym"))) %>%
-      mutate(issued_year = lubridate::year(issued))
-  } else {
-   out <- NULL
-  }
+    mutate(issued_year = lubridate::year(issued))
   out
 }
 

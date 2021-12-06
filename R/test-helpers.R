128d848c02281ab4f70e3753af617bdc73a6d132
Maximilian Held <info@maxheld.de>
2021-12-06 21:02:43 +0100

    migrate out test helper

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -2,3 +2,10 @@
 #' Recommended by https://stackoverflow.com/questions/1368163/is-there-a-standard-domain-for-testing-throwaway-email
 #' @noRd
 throwaway <- "whatever@mailinator.com"
+
+#' used often b/c plain json doesn't cover complex objects
+#' @noRd
+expect_snapshot_value2 <- purrr::partial(
+  testthat::expect_snapshot_value,
+  style = "json2"
+)
--- tests/testthat/setup.R
+++
@@ -1,2 +0,0 @@
-# used often b/c plain json doesn't cover complex objects
-expect_snapshot_value2 <- purrr::partial(expect_snapshot_value, style = "json2")

0b1c546b43eb693b2370af021fdc3792d11ea0ee
Maximilian Held <info@maxheld.de>
2022-01-14 19:53:07 +0100

    fix path

--- inst/saio/saio_launcher.R
+++ inst/saio/saio_launcher.R
@@ -1,5 +1,5 @@
 # packrat dark magic does not recognise the subugoetheme dependency
 # this hack fixes that by just calling some random function
 subugoetheme::ejd_pal()
-source("env_secrets.R")
+source(metacheck:::system_file2("saio", "env_secrets.R"))
 shiny::shinyApp(ui = metacheck:::mcAppUI(), server = metacheck:::mcAppServer)

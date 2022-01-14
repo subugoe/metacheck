960ce3bcf5cbc052d708f405c590b9a9d80835fd
Maximilian Held <info@maxheld.de>
2022-01-14 19:39:41 +0100

    write secrets to file

--- R/deploy.R
+++ R/deploy.R
@@ -13,6 +13,25 @@ deployAppSaio <- function() {
   )
 }
 
+#' Helper to write env secret vars to file on saio
+#' This is a pretty bad hack,
+#' but saio does not have secret env vars
+#' @noRd
+write_env_var <- function(name) {
+  value <- Sys.getenv(name)
+  env_file <- fs::path(system_file2("saio"), "env_secrets.R")
+  if (!fs::file_exists(env_file)) {
+    fs::file_create(env_file)
+  }
+  write(
+    x = glue::glue(
+      "Sys.setenv('{name}' = '{value}')"
+    ),
+    file = fs::path(system_file2("saio", "env_secrets.R")),
+    append = TRUE
+  )
+}
+
 #' Install metacheck dep again
 #' @noRd
 install_for_packrat <- function() {
--- inst/saio/saio_launcher.R
+++ inst/saio/saio_launcher.R
@@ -1,7 +1,5 @@
-metacheck::auth_mailjet()
-metacheck::auth_mailjet()
 # packrat dark magic does not recognise the subugoetheme dependency
 # this hack fixes that by just calling some random function
 subugoetheme::ejd_pal()
-Sys.getenv("MAILJET_SMTP_PASSWORD")
+source("env_secrets.R")
 shiny::shinyApp(ui = metacheck:::mcAppUI(), server = metacheck:::mcAppServer)

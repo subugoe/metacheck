48a02e8ae3f42038326a23f33fcbc58482ac8a9d
Maximilian Held <info@maxheld.de>
2022-01-14 18:22:34 +0100

    pass secret env var into saio

--- inst/saio/saio_launcher.R
+++ inst/saio/saio_launcher.R
@@ -3,4 +3,5 @@ metacheck::auth_mailjet()
 # packrat dark magic does not recognise the subugoetheme dependency
 # this hack fixes that by just calling some random function
 subugoetheme::ejd_pal()
+Sys.getenv("MAILJET_SMTP_PASSWORD")
 shiny::shinyApp(ui = metacheck:::mcAppUI(), server = metacheck:::mcAppServer)

f39d4d27a0e570850d05a98087b5061d16238809
Maximilian Held <info@maxheld.de>
2022-01-15 00:58:17 +0100

    define cache at runtime

--- R/import.R
+++ R/import.R
@@ -132,10 +132,6 @@ cache_api <- function() {
   }
 }
 
-#' Cache results
-#' @noRd
-memoised_cr_works <- memoise::memoise(insistently_cr_works, cache = cache_api())
-
 #' Retrieve some field
 #' This needs to call memoised version,
 #' because we want to always re-use the same entire result object.
@@ -153,29 +149,6 @@ possibly_cr_works_field <- purrr::possibly(
   cr_works_field,
   otherwise = NA,
   quiet = !interactive()
-)
-
-#' Cache results
-#' @details
-#' possibly_cr_works_field also needs to be cached
-#' so that `NA` results are cached.
-#' Above, lower level caching can only cache when there *is* a result
-#' Conditions cannot be cached.
-#' @noRd
-memoised_possibly_cr_works_field <- memoise::memoise(
-  possibly_cr_works_field,
-  cache = cache_api()
-)
-
-# also cache biblids calls to disc if possible
-# pretty bad hack, because this makes it double memoised
-memoised_is_doi_found <- memoise::memoise(
-  biblids::is_doi_found,
-  cache = cache_api()
-)
-memoised_is_doi_from_ra <- memoise::memoise(
-  biblids::is_doi_from_ra,
-  cache = cache_api()
 )
 
 #' Helper for use in predicates.
--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,31 @@
 .onLoad <- function(libname, pkgname) {
   auth_cr()
   cache_api()
+
+  #' Cache results
+  #' @details
+  #' possibly_cr_works_field also needs to be cached
+  #' so that `NA` results are cached.
+  #' Above, lower level caching can only cache when there *is* a result
+  #' Conditions cannot be cached.
+  #' @noRd
+  memoised_possibly_cr_works_field <<- memoise::memoise(
+    possibly_cr_works_field,
+    cache = cache_api()
+  )
+
+  # also cache biblids calls to disc if possible
+  # pretty bad hack, because this makes it double memoised
+  memoised_is_doi_found <<- memoise::memoise(
+    biblids::is_doi_found,
+    cache = cache_api()
+  )
+  memoised_is_doi_from_ra <<- memoise::memoise(
+    biblids::is_doi_from_ra,
+    cache = cache_api()
+  )
+
+  #' Cache results
+  #' @noRd
+  memoised_cr_works <<- memoise::memoise(insistently_cr_works, cache = cache_api())
 }
